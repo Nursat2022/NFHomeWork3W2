0e35a5870dbc28cd1907d68fc82479bb30e888a6
Nursat2022 <nursatsakyshev2003@gmail.com>
2023-06-20 22:26:30 +0600

    hide keyboard

--- WaterBalaceUI/WaterBalaceUI/DailyIntake.swift
+++ WaterBalaceUI/WaterBalaceUI/DailyIntake.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct DailyIntake: View {
     @Environment(\.presentationMode) var presentationMode
+    @State private var keyboardHeight: CGFloat = 0
     @EnvironmentObject var settingsData: SettingsData
     @State var toMain = false
     @State var volume = "\(String(format: "%.0f", AppDataAPI.dailyIntake))"
@@ -21,28 +22,48 @@ struct DailyIntake: View {
                     .fontWeight(.black)
                     .foregroundColor(Color(red: 5/255, green: 165/255, blue: 239/255))
             }
-            Headers(text: "Daily intake?")
-            TextFieldForVolume(volume: $volume)
-            nextOrSaveButton(text: "Save", action: {
-                AppDataAPI.dailyIntake = Double(volume) ?? 0
-                settingsData.dailyIntake = AppDataAPI.dailyIntake
-                if AppDataAPI.isOnboarding {
-                    self.presentationMode.wrappedValue.dismiss()
+            Group {
+                Headers(text: "Daily intake?")
+                TextFieldForVolume(volume: $volume)
+                nextOrSaveButton(text: "Save", action: {
+                    AppDataAPI.dailyIntake = Double(volume) ?? 0
+                    settingsData.dailyIntake = AppDataAPI.dailyIntake
+                    if AppDataAPI.isOnboarding {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }
+                    else {
+                        AppDataAPI.isOnboarding = true
+                        toMain = true
+                    }
+                })
+            }
+            .offset(y: -keyboardHeight + 30)
+            .animation(.easeInOut(duration: 0.3))
+            .onAppear {
+                NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { notification in
+                    if let keyboardSize = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+                        keyboardHeight = keyboardSize.height
+                    }
                 }
-                else {
-                    AppDataAPI.isOnboarding = true
-                    toMain = true
+                
+                NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) { _ in
+                    keyboardHeight = 0
                 }
-            })
+            }
+            
             NavigationLink(isActive: $toMain) {
                 Main()
                     .navigationBarBackButtonHidden(true)
             } label: {}
-
+            
         }
         .padding(.top, AppDataAPI.isOnboarding ? 56 : 108)
         .padding(.bottom, 50)
         .ignoresSafeArea()
+    }
+    
+    private func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
@@ -56,7 +77,7 @@ struct TextFieldForVolume: View {
             
             HStack(spacing: 12) {
                 TextField("", text: $volume)
-                    .keyboardType(.decimalPad)
+                    .keyboardType(.numberPad)
                     .foregroundColor(.blue)
                     .padding(.horizontal, 19)
                     .frame(width: 250, height: 60)
